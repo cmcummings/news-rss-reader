1c7c1e1dcb9fa81429986912191a00f16e7749fb
Connor Cummings <cmcummings2003@gmail.com>
2023-02-19 12:12:37 -0600

    Restored dependencies, updated for pages

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:cmcummings/news-rss-reader.git main:gh-pages
+
+cd -
